256e810a5fb72ced5fbeda50e79c10f7815bafbe
jvvaccari <jv_vaccari@live.com>
2025-11-27 12:33:41 -0300

    feat: adicionar controlador AppController ao módulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 import { PolaroidModule } from './modules/polaroid/polaroid.module';
 import { DailyChallengeModule } from './modules/daily-challenge/daily-challenge.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
+  controllers: [AppController],
   imports: [PrismaModule, PolaroidModule, DailyChallengeModule],
 })
 export class AppModule {}
